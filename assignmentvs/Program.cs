a1ec809818fc4663a1c1da4f1765ceac54019349
Minh3782 <tle276@my.centennialcollege.ca>
2024-10-11 13:31:41 -0400

    Add files via upload

---
+++ assignmentvs/Program.cs
@@ -0,0 +1,127 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"sum: {a + b}");
+//string userName = Console.ReadLine();
+//userName = userName.Replace("@", ".");
+//Console.WriteLine(userName);
+
+
+//QUESTION 1
+string plusLine = "+" * 20;
+Console.WriteLine(plusLine);
+Console.WriteLine("+\tCentennial College\t+");
+Console.WriteLine("+\t941 Progress Avenue");
+Console.WriteLine("+\tScarborough, ON \t+");
+Console.WriteLine(plusLine);
+
+
+//Console.ReadLine();
+
+
+//QUESTION 2
+//Console.Write("Enter the item: ");
+//string itemName = Console.ReadLine();
+//Console.Write("The price of the item: ");
+//double itemPrice = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine($"You have to pay: {1.13 * itemPrice:c} for a {itemName}");
+//Console.ReadLine();
+
+
+
+//QUESTION 3
+//Console.WriteLine("Enter a double: ");
+//double number = Convert.ToDouble(Console.ReadLine());
+//char conLetter = Convert.ToChar(Convert.ToInt32(number));
+//Console.WriteLine($"The number is {number} that equivalent to the character {conLetter}");
+
+
+
+//QUESTION 4
+//Console.Write("Enter the amount of your purchase: ");
+//double cost = Convert.ToDouble(Console.ReadLine()),
+//            penalty = 0.05 * cost;
+
+//if (cost > 500)
+//{
+//    Console.WriteLine($"Cost: {cost:c}, penalty: {penalty:c} so you pay {cost + penalty:c}");
+//}
+//else
+//{
+//    Console.WriteLine($"The cost: {cost:c}, penalty: $0.00 so you pay {cost:c}");
+//}
+
+
+//QUESTION5
+//Console.Write("What is your home planet? ");
+//string planetName = Console.ReadLine();
+//if (planetName == "earth" || planetName == "VENUS" || planetName == "Mars")
+//{
+//    switch (planetName)
+//    {
+//        case "earth":
+//            Console.WriteLine("earth. You are an Earthling and you have 10 fingers");
+//            break;
+//        case "VENUS":
+//            Console.WriteLine("VENUS. You are a Venusian and you have 12 fingers");
+//            break;
+//        case "Mars":
+//            Console.WriteLine("Mars. You are a Martian and you have 8 fingers");
+//            break;
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("I am sorry I don't know of that planet");
+//}
+
+
+//QUESTION 6
+//Console.Write("What is kind of car would you like to rent: ");
+//string vehicleName = Console.ReadLine();
+//if (vehicleName == "sedan" || vehicleName == "COMPACT" || vehicleName == "Sports")
+//{
+//    switch(vehicleName)
+//    {
+//        case "sedan":
+//            Console.WriteLine("sedan cost $21.40 per day");
+//            break;
+//        case "COMPACT":
+//            Console.WriteLine("COMPACT cost $18.90 per day");
+//            break;
+//        case "Sports":
+//            Console.WriteLine("Sports cost $32.80 per day");
+//            break;
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("I am sorry I don't know of that kind of car");    
+//}
+
+
+Console.ReadLine();
+
+//Console.Write("What is your home planet? ");
+//string earth = Console.ReadLine(),
+//          venus = Console.ReadLine(),
+//          mars = Console.ReadLine();
+//if ("earth" || "venus" || "mars")
+//{
+//    switch (planetName)
+//    {
+//        case "earth":
+//            Console.WriteLine("earth. You are an Earthling and you have 10 fingers");
+//            break;
+//        case "VENUS":
+//            Console.WriteLine("VENUS. You are a Venusian and you have 12 fingers");
+//            break;
+//        case "Mars":
+//            Console.WriteLine("Mars. You are a Martian and you have 8 fingers");
+//            break;
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("I am sorry I don't know of that planet");
+//}
